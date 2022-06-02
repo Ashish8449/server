7e35dd2646ac5e6d71ca535ff667337254212b47
ashish8449 <ashishgururani8449@gmail.com>
2022-06-02 10:28:43 +0530

    commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({
+    hi: "djfkdfj",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("server started runing on port 5000");
+});
